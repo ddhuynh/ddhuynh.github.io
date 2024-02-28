8525dd726f7624c48055df637bc2ec559c199c7a
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-02-28 11:15:04 -0500

    Updated src files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Homepage from './pages/Homepage'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
@@ -8,9 +7,6 @@ function App() {
   return (
     <>
         <Homepage />
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-        </h1>
     </>
   )
 }
