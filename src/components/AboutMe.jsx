202b6796e3bf330a1d8713a1dc2f08e5460cca37
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-08-04 18:34:05 -0400

    Created about me component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,16 @@
+
+function AboutMe()
+{
+  return (
+      <div className="sectionContainer">
+        <div className="aboutContent">
+          <h1>Hello World!</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+  )
+}
+
+export default AboutMe;
+
+// Using text-align instead of align-items fixes the issue with highlighting text.
