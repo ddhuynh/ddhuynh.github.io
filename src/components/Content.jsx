3ee19ae6b2408943052ed5dff15f5b2609fce041
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-08-05 11:42:08 -0400

    Updated formatting of content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,28 @@
 import TempImage from "../assets/TempImage.png"
+import content from "../assets/postContent.json"
 
 function Content()
 {
+  const displayContent = content.map((postItem, index) => (
+    <div key={index} className="postContent">
+      <h2>{postItem.title}</h2>
+      <h4>{postItem.date}</h4>
+      <p>{postItem.description}</p>
+      <img src={TempImage}></img>
+    </div>
+  ));
+
+
+
   return (
-      <>
-        <div className="postItem">
-            <b><p>Project</p></b>
-        </div>
-
-
-        <div className="postItem">
-            <b><p>PROJ</p></b>
-        </div>
+    <>
+      {/* {displayContent} */}
+      <div className="postContent">
+          <h2>Hello World!!!</h2>
+          <h4>Date: 9000000000</h4>
+          <p>Short Description</p>
+          <img src={TempImage}></img>
+      </div>
     </>
   )
 }
