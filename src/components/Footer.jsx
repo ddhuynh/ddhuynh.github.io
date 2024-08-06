22d4d1bd30fccb90d7bdf3ce3fee2a8a6e62e6c1
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-08-06 00:34:03 -0400

    Added basic icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+
 
 function Footer()
 {
   return (
     <div className="sectionContainer">
       <div className="footerContent">
-        <p>FOOTER TO BE ADDED!</p>
+        <FaLinkedin size={30}/>
+        <a href="https://github.com/DaviHynh" target="_blank" rel="noopener noreferrer">
+          <FaGithubSquare size={30}/>
+        </a>
       </div>
     </div>
   )
