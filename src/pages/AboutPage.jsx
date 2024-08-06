4f74e44aedb9471009b3a5d8937719d8326c35f3
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-08-06 16:17:07 -0400

    Created separate page for about me

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import Banner from "../components/Banner";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
+import AboutMe from "../components/AboutMe";
+
+export default function AboutPage()
+{
+  return (
+    <>
+      <div className="pageContainer">
+        <Banner />
+        
+        <Footer />
+      </div>
+    </>
+  )
+}
