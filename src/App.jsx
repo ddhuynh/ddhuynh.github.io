d8026af69bb00ffcc530ff2835aae4b4be7f6f7f
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-08-06 16:15:37 -0400

    Added routing via react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
