78777050d45c8c6122ceb8355489f9fb468f46c6
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2025-04-10 13:39:22 -0400

    Revised content to tsx file. Added typings as necessary.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,34 @@
+// In this file, I'm taking info from my json file and mapping it out for display.
+// If you want to modify how stuff per block looks, you can do it here.
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 
 import CardFlip from './CardFlip';
+
+// This json file is an array of json objects. Each object contains information for each card.
 import content from "../assets/postContent.json"
+
+interface ObjectContent
+{
+  title: string;
+  date: string;
+  description: string;
+  link: string;
+  image: string;
+  moreInfo: string;
+}
 
 function Content()
 {
-  const [showModal, setShowModal] = useState(false);
-  const [path, setPath] = useState("");
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [path, setPath] = useState<string>("");
 
-  const displayContent = content.map((postItem, index) => (
+  // I have an array of json objects (content). I'm mapping the content of that in the function below.
+  // This takes a json object in the array, the index of the array, and maps it accordingly.
+  const displayContent = content.map((postItem: ObjectContent, index: number) => (
       <div className='contentContainer' key={index}>
         <div className='contentImageContainer'>
-          {/* This is a component that creates a flippable card. It uses postItem.image. */}
+          {/* Here, we send our singular json object to another component, which handles card flipping.*/}
           <CardFlip postItemProp={postItem} setShowModalProp={setShowModal} setPathProp={setPath} />
         </div>
 
@@ -30,6 +46,7 @@ function Content()
     <>
       {displayContent}
 
+      {/* This is just a modal from bootstrap that I use to bring up a larger version of the image. */}
       <Modal show={showModal} onHide={() => setShowModal(false)} dialogClassName='customModalClass' centered>
         <Modal.Body onClick={() => setShowModal(false)} style={{padding: "0px"}}>
           <img className='modalImage' src={path}></img>
